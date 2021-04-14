3e4e782127f0623bc65c22fe9f2df6a715330040
feliperuben <feliperuben@mppa.mp.br>
2021-04-14 10:20:56 -0300

    botao de pegar mais piadas funcionando

--- src/JokeList.js
+++ src/JokeList.js
@@ -10,25 +10,44 @@ export default class JokeList extends Component {
   };
   constructor(props) {
     super(props);
-    this.state = { jokes: [] };
+    //pega as piadas do localstorage ou retorna um array vazio
+    this.state = { jokes: JSON.parse(window.localStorage.getItem("jokes") || "[]") };
+    this.handleClick = this.handleClick.bind(this);
   }
   async componentDidMount() {
+    if(this.state.jokes.length === 0) this.getJokes();
+  }
+  async getJokes() {
     let jokes = [];
+    //faz o fetch e preenche o array com piadas
     while(jokes.length < this.props.numJokesToGet) {
       let res = await axios.get("https://icanhazdadjoke.com/", {
         headers: {Accept: "application/json"}
       });
       jokes.push({ id: uuid(), text: res.data.joke, votes: 0 })
     }
-    this.setState({ jokes: jokes }) 
+    this.setState(st => ({
+      jokes: [...st.jokes, ...jokes]
+    }),
+    () => window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes)));
+    //guarda no local storage as piadas e converte para string
+    window.localStorage.setItem(
+      "jokes",
+      JSON.stringify(jokes)
+    );
   }
   handleVote(id, delta) {
     this.setState(
       st => ({
         jokes: st.jokes.map(j =>
           j.id === id ? { ...j, votes: j.votes + delta } : j)
-      })
+      }),
+      //resolve o bug de localstorage dos votos
+      () => window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
     )
+  }
+  handleClick() {
+    this.getJokes();
   }
 
   render() {
@@ -37,7 +56,7 @@ export default class JokeList extends Component {
         <div className="JokeList-sidebar">
           <h1 className="JokeList-title"><span>Dad</span> Jokes</h1>
           <img src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg" />
-          <button className="JokeList-getMore">New Jokes</button>
+          <button className="JokeList-getMore" onClick={this.handleClick()}>New Jokes</button>
         </div>
 
         <div className="JokeList-jokes">
